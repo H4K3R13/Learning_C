96902416b650bdbe24fdc9607134fa0c01142aac
H4K3R13 <50370450+H4K3R13@users.noreply.github.com>
2021-04-21 18:20:40 +0530

    Add files via upload

---
+++ Program List/Q8CubeRoot.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int x
+    printf("Enter the integer to find the cube root:  ");
+    scanf("%d",&x);
+    for(int i=0;i < x;i++)
+      {
+        cube=i*i*i;
+        if(cube==x)
+          {
+            printf("\nCube root of %d is %d:",x,i);
+            break;
+          }
+        else if(cube>x)
+          {
+            printf("\n Number is not a perfect cube.");
+            break;
+          }
+      }
+}
