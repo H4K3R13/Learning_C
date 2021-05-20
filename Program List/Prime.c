e21b7872fba6a7b06bc9c6bc36459ee69da92c38
H4K3R13 <50370450+H4K3R13@users.noreply.github.com>
2021-05-20 09:48:17 +0530

    Add files via upload

---
+++ Program List/Prime.c
@@ -0,0 +1,33 @@
+/*Write a program to print all prime numbers from 1 to 300. 
+(Use nested loops, break and continue)*/
+#include <stdio.h>
+int main()
+{
+  int n, i = 3, count, c;
+
+  printf("Enter the number of prime numbers to print\n");
+  scanf("%d", &n);
+
+  if (n >= 1) {
+    printf("First %d prime numbers are:\n",n);
+    printf("2\n");
+  }
+
+  for (count = 2; count <= n;)
+  {
+    for (c = 2; c <= i - 1; c++)
+    {
+      if (i%c == 0)
+        break;
+    }
+    if (c == i)
+    {
+      printf("%d\n", i);
+      count++;
+    }
+    i++;
+  }
+
+  return 0;
+}
+
